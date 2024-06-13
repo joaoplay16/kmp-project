7de06f6e0ee3e79a1d5b0fd65d0a135b14a2c5fd
João Pedro <joaoplay16@gmail.com>
2024-06-13 20:35:41 -0300

    Implement tooltips

--- composeApp/src/desktopMain/kotlin/screen/ExampleScreen.kt
+++ composeApp/src/desktopMain/kotlin/screen/ExampleScreen.kt
@@ -4,10 +4,16 @@ import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.BottomEnd
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
@@ -16,7 +22,8 @@ class ExampleScreen : Screen {
     @Composable
     override fun Content() {
 //        ScrollableList()
-        ScrollableLazyList()
+//        ScrollableLazyList()
+          Tooltips()
     }
 
     @Composable
@@ -102,4 +109,46 @@ class ExampleScreen : Screen {
         }
     }
 
+    @OptIn(ExperimentalFoundationApi::class)
+    @Composable
+    fun Tooltips() {
+        val buttons = listOf("Contact us", "About")
+
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            buttons.forEachIndexed { index, title ->
+                TooltipArea(
+                    tooltip = {
+                        Surface(
+                            modifier = Modifier.shadow(8.dp),
+                            color = Color.LightGray,
+                            shape = RoundedCornerShape(8.dp)
+                        ) {
+                            Text(
+                                modifier = Modifier.padding(10.dp),
+                                text = if (index == 0) " Get in touch"
+                                else "This is our team",
+                            )
+                        }
+                    },
+                    delayMillis = 600,
+                    tooltipPlacement = TooltipPlacement.CursorPoint(
+                        alignment = BottomEnd
+                    )
+                ) {
+                    Button(onClick = {}) {
+                        Text(text = title)
+                    }
+                }
+                if (index == 0) {
+                    Spacer(
+                        modifier = Modifier.height(12.dp)
+                    )
+                }
+            }
+        }
+    }
 }

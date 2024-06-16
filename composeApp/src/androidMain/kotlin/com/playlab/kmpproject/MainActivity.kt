b890d9020736cb4330bc3f68a56b23a09deecbb4
João Pedro <joaoplay16@gmail.com>
2024-06-15 22:05:32 -0300

    Change content of the android part

--- composeApp/src/androidMain/kotlin/com/playlab/kmpproject/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/playlab/kmpproject/MainActivity.kt
@@ -1,18 +1,18 @@
 package com.playlab.kmpproject
 
-import App
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.material3.Text
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            App()
+            Text("Android is better than ios shit")
         }
     }
 }
@@ -20,5 +20,5 @@ class MainActivity : ComponentActivity() {
 @Preview
 @Composable
 fun AppAndroidPreview() {
-    App()
+    Text("Something")
 }

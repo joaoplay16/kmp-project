ccce7153179ebbc1f21ba589b758212c68ad2709
João Pedro <joaoplay16@gmail.com>
2024-06-18 20:05:06 -0300

    Implement navigate back with backspace

--- composeApp/src/desktopMain/kotlin/NavigationControls.kt
+++ composeApp/src/desktopMain/kotlin/NavigationControls.kt
@@ -1,3 +1,4 @@
+import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
@@ -8,9 +9,20 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusOrder
+import androidx.compose.ui.focus.focusProperties
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.input.key.KeyEventType
+import androidx.compose.ui.input.key.key
+import androidx.compose.ui.input.key.onKeyEvent
+import androidx.compose.ui.input.key.type
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.navigator.Navigator
+import androidx.compose.ui.input.key.*
 
 @Composable
 fun NavigationControls(
@@ -18,7 +30,22 @@ fun NavigationControls(
     navigator: Navigator,
     content: @Composable () -> Unit
 ) {
-    Column(modifier.padding(horizontal = 16.dp)) {
+    val focusRequester = remember { FocusRequester() }
+
+    LaunchedEffect(Unit) {
+        focusRequester.requestFocus()
+    }
+
+    Column(modifier
+        .focusable(true)
+        .focusRequester(focusRequester)
+        .onKeyEvent {
+            if (it.key == Key.Backspace && it.type == KeyEventType.KeyDown) {
+                navigator.pop()
+                false
+            } else false
+        }
+        .padding(horizontal = 16.dp)) {
         Row {
             IconButton(
                 onClick = {

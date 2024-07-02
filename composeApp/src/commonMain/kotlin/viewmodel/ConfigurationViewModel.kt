d62a69181df8b950f426e52576ce092f80f8756d
João Pedro <joaoplay16@gmail.com>
2024-07-01 22:31:01 -0300

    Use the dataStore instance to save the settings

--- composeApp/src/commonMain/kotlin/viewmodel/ConfigurationViewModel.kt
+++ composeApp/src/commonMain/kotlin/viewmodel/ConfigurationViewModel.kt
@@ -3,11 +3,24 @@ package viewmodel
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import data.local.PreferencesDataStore
+import kotlinx.coroutines.launch
 
-class ConfigurationViewModel : ViewModel() {
+class ConfigurationViewModel(private val dataStore: PreferencesDataStore) : ViewModel() {
     val darkTheme: State<Boolean> field = mutableStateOf(false)
 
+    init {
+        viewModelScope.launch {
+            dataStore.isDarkThemeEnabled.collect {
+                darkTheme.value = it
+            }
+        }
+    }
+
     fun toggleDarkTheme() {
-        darkTheme.value = !darkTheme.value
+        viewModelScope.launch {
+            dataStore.setDarkThemeEnabled(!darkTheme.value)
+        }
     }
 }
